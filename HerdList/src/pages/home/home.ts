1a8553baea0a43044861881f03da95501e62e1ce
DerrickConway <derrick.conway15@gmail.com>
2017-02-13 19:21:38 +0000

    connect to database
    onnect to database

--- HerdList/src/pages/home/home.ts
+++ HerdList/src/pages/home/home.ts
@@ -1,15 +1,54 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+export class HomePage {
+  herds: FirebaseListObservable<any>;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, angFire: AngularFire) {
+      this.herds = angFire.database.list('/HerdLists');
+  }
+
+  addList():void {
+    let prompt = this.alertCtrl.create({
+        title: 'Herd Info:',
+      message: 'Enter Herd Information',
+      inputs:[
+        {
+          name: 'tagNumber',
+          placeholder: "Tag Number"
+        },
+        {
+          name: 'numberOfCalfs',
+          placeholder: "Number Of Calfs"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          handler: data => {
+            console.log("cancel clicked");
+          }
+        },
+        {
+          text: "Save",
+          handler: data => {
+            this.herds.push({
+              tagNumber: data.tagNumber,
+                numberOfCalfs: data.numberOfCalfs
+            })
+          }
+        }
+      ]
+    });
+
+    prompt.present();
   }
 
 }
