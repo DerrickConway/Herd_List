c41cf709b535cd9bf3148c56d9a549a88df3d65d
DerrickConway <derrick.conway15@gmail.com>
2017-02-22 13:44:48 +0000

    edit and delete added
    edit and delete added

--- HerdList/src/pages/home/home.ts
+++ HerdList/src/pages/home/home.ts
@@ -25,9 +25,22 @@ export class HomePage {
           placeholder: "Tag Number"
         },
         {
-          name: 'numberOfCalfs',
-          placeholder: "Number Of Calfs"
+          name: 'numberOfCalves',
+          placeholder: "Number Of Calves"
+        },
+        {
+          name: 'dob',
+          placeholder: "Date Of Birth"
+        },
+        {
+          name: 'breeds',
+          placeholder: "Breed"
+        },
+        {
+          name: 'comp',
+          placeholder: "Complications"
         }
+
       ],
       buttons: [
         {
@@ -41,8 +54,109 @@ export class HomePage {
           handler: data => {
             this.herds.push({
               tagNumber: data.tagNumber,
-                numberOfCalfs: data.numberOfCalfs
+                numberOfCalves: data.numberOfCalves,
+              dob: data.dob,
+              breeds: data.breeds,
+              comp: data.comp
+
             })
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+  editHerd(herd):void {
+    let prompt = this.alertCtrl.create({
+      title: 'Edit Herd',
+      message: 'Edit the Herd Information',
+      inputs:[
+        {
+          name: 'tagNumber',
+          placeholder: herd.tagNumber
+        },
+        {
+          name: 'numberOfCalves',
+          placeholder: herd.numberOfCalves
+        },
+        {
+          name: 'dob',
+          placeholder: herd.dob
+        },
+        {
+          name: 'breeds',
+          placeholder: herd.breed
+        },
+        {
+          name: 'comp',
+          placeholder: herd.comp
+        }
+
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          handler: data => {
+            console.log("cancel clicked");
+          }
+        },
+        {
+          text: "Save",
+          handler: data => {
+            let newTagNumber:String = herd.tagNumber;
+            let newNumberOfCalves:String = herd.numberOfCalves;
+            let newdob:String = herd.dob;
+            let newBreeds:String = herd.breeds;
+            let newComp:String = herd.comp;
+
+            if(data.tagNumber != ''){
+              newTagNumber = data.tagNumber;
+            }
+            if(data.numberOfCalves != ''){
+              newNumberOfCalves = data.numberOfCalves;
+            }
+            if(data.dob != ''){
+              newdob = data.dob;
+            }
+            if(data.breeds != ''){
+              newBreeds = data.breeds;
+            }
+            if(data.comp != ''){
+              newComp = data.comp;
+            }
+
+            this.herds.update(herd.$key,{
+              tagNumber: newTagNumber,
+              numberOfCalves: newNumberOfCalves,
+              dob: newdob,
+              breeds: newBreeds,
+              comp: newComp
+
+            })
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+  deleteHerd(herdID):void {
+    let prompt = this.alertCtrl.create({
+      title: 'Delete Herd Info:',
+      message: 'Delete Herd Information',
+
+      buttons: [
+        {
+          text: "Cancel",
+          handler: data => {
+            console.log("cancel clicked");
+          }
+        },
+        {
+          text: "Delete Info",
+          handler: data => {
+            this.herds.remove(herdID);
           }
         }
       ]
